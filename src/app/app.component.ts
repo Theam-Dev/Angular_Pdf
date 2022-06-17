f9abba807a6c2c0fcdb5e97562ba0451152f258e
Pin Phearak <phearakroun@gmail.com>
2022-06-18 01:24:23 +0700

    Angular Export Pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import {formatDate} from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pdf';
+  date = formatDate(new Date(), 'yyyy/MM/dd', 'en');
+  fileName= 'pdf -'+this.date+'.pdf';
+  public exportexpdf() : void
+  {
+    let DATA: any = document.getElementById('pdf-table');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save(this.fileName);
+    });
+
+  }
 }
